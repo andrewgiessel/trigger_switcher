07400c727379419e325da704db922393396ff860
andrew giessel <andrew.giessel@gmail.com>
2013-08-12 15:58:08 -0500

    Update passthru.ino
    broke out update thru pin to seperate function
    
    added 10 ms delay to deal with switch bounce, remove later
    
    note that strings terminate with \0 - had to add a case statement to treat it as a '0'
    
    added code to swap pins (valve 0 on/off at the same time as other valve off/on)
    
    used memset to set all the memory in the the triggerSequence buffer to '0'

--- passthru/passthru.ino
+++ passthru/passthru.ino
@@ -5,6 +5,11 @@ int outPin = 8;
 int triggerVal = 0;
 char inputVal = '0';
 int blinkVal = 5;
+boolean wasTriggered = 0;
+
+char triggerSequence[2048];
+
+
 int triggerPosition = 0;
 
 void setup() {
@@ -29,12 +34,44 @@ void setup() {
    pinMode(42, OUTPUT);
    pinMode(41, OUTPUT);
    
+   digitalWrite(7, HIGH);
+   memset(triggerSequence, '0', sizeof(triggerSequence));
    Serial.begin(9600);
  }
 
 void loop(){
   if (Serial.available()>0) {
-    inputVal = Serial.read();
+    memset(triggerSequence, '0', sizeof(triggerSequence));
+    Serial.readBytesUntil('\n', triggerSequence, 2048);
+    Serial.write(triggerSequence);
+    Serial.write('\n');
+    
+    triggerPosition = 0;
+    updateThruPin();
+    digitalWrite(7, HIGH); // reset so valve 0 is on
+  }
+  
+  triggerVal = digitalRead(triggerPin);
+  if (triggerVal == HIGH) {
+    // set valve 0 to off
+    digitalWrite(7, LOW);
+    digitalWrite(outPin, HIGH);
+    delay(10);
+    wasTriggered = 1;
+  } else {
+    digitalWrite(outPin, LOW);
+    digitalWrite(7,HIGH);
+    delay(10);
+    if (wasTriggered) {
+      triggerPosition += 1;
+      updateThruPin();
+      wasTriggered = 0;
+    }
+  }
+}
+
+void updateThruPin() {
+    inputVal = triggerSequence[triggerPosition];
     
     switch (inputVal) {
       case '0':
@@ -85,17 +122,10 @@ void loop(){
       case 'f':
         outPin = 41;
         break;
+      case '\0':
+        outPin = 7;
+        break;
     }
-    
     Serial.println(outPin);
-  
-  }
-  
-  triggerVal = digitalRead(triggerPin);
-  if (triggerVal == HIGH) {
-    digitalWrite(outPin, HIGH);
-  } else {
-    digitalWrite(outPin, LOW);
-  }
+    Serial.flush();
 }
-
