06f97d64d48addb0868ecc61adce65f15dce425a
andrew giessel <andrew.giessel@gmail.com>
2013-08-13 16:10:34 -0500

    Update passthru.ino

--- passthru/passthru.ino
+++ passthru/passthru.ino
@@ -1,23 +1,24 @@
+/*
+
+*/
+
+
 const int triggerPin = 4;
-const int ledPin = 6;
 
 int outPin = 8;
 int triggerVal = 0;
 char inputVal = '0';
-int blinkVal = 5;
+
 boolean wasTriggered = 0;
 
 char triggerSequence[2048];
-
-
 int triggerPosition = 0;
 
 void setup() {
    pinMode(triggerPin, INPUT);
-   pinMode(ledPin, OUTPUT);
-   
+
    // set pins as out
-   pinMode(7, OUTPUT);
+   pinMode(7, OUTPUT); // valve 0
    pinMode(8, OUTPUT);
    pinMode(9, OUTPUT);
    pinMode(10, OUTPUT);
@@ -32,36 +33,60 @@ void setup() {
    pinMode(44, OUTPUT);
    pinMode(43, OUTPUT);
    pinMode(42, OUTPUT);
-   pinMode(41, OUTPUT);
+   pinMode(41, OUTPUT); // valve 15
    
+   // set valve 0 to on by default
    digitalWrite(7, HIGH);
+   
+   // clear all values in the trigger sequence
    memset(triggerSequence, '0', sizeof(triggerSequence));
+
    Serial.begin(9600);
  }
 
+/* 
+loop has two stages
+
+1)  check for new serial input that indicates a series of states.
+    This will be a string of hexidecimal characters, 0-9 or a-f
+    These will be mapped to valve positions, 0-15.  Saved as a 
+    character array in 'triggerSequence'.  Current valve is saved
+    as an index called 'triggerPosition'
+    
+2)  Check the state of the triggerPin and pass thru it's state to
+    the correct pin, and turn off valve 0.  This will continually 
+    be set through each time in the loop, which is wasteful but no 
+    big deal and makes signal generation easy.  Once high, mark 
+    'wasTriggered' as true.
+    
+    If the pin is low, and wasTriggered is true, then we've transitioned
+    out.  Turn on valve 0, turn off the current valve, and increment
+    the triggerPosition.  
+*/      
 void loop(){
+  // serial
   if (Serial.available()>0) {
     memset(triggerSequence, '0', sizeof(triggerSequence));
-    Serial.readBytesUntil('\n', triggerSequence, 2048);
-    Serial.write(triggerSequence);
-    Serial.write('\n');
-    
     triggerPosition = 0;
+
+    Serial.readBytesUntil('\n', triggerSequence, 2048);
+    
     updateThruPin();
     digitalWrite(7, HIGH); // reset so valve 0 is on
   }
   
+  // pin logic
   triggerVal = digitalRead(triggerPin);
   if (triggerVal == HIGH) {
     // set valve 0 to off
     digitalWrite(7, LOW);
     digitalWrite(outPin, HIGH);
-    delay(10);
+    //delay(10);
     wasTriggered = 1;
   } else {
     digitalWrite(outPin, LOW);
     digitalWrite(7,HIGH);
-    delay(10);
+    //delay(10);
     if (wasTriggered) {
       triggerPosition += 1;
       updateThruPin();
@@ -70,6 +95,7 @@ void loop(){
   }
 }
 
+// this code is to map serial character to the right pin for each valve
 void updateThruPin() {
     inputVal = triggerSequence[triggerPosition];
     
@@ -122,10 +148,15 @@ void updateThruPin() {
       case 'f':
         outPin = 41;
         break;
-      case '\0':
+      case '\0':  // string termination: valve 0
+        outPin = 7;
+        break;
+      default: // anything weird:  valve 0
         outPin = 7;
         break;
     }
+    Serial.print("Next pin: ");
     Serial.println(outPin);
+    
     Serial.flush();
 }
